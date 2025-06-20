2ba83cf249c11a37478f80475bfbb50921806cd3
Samir <samirelorf@yahoo.fr>
2025-06-20 15:27:05 +0200

    first commit

---
+++ bonjour_javascript/script.js
@@ -0,0 +1,77 @@
+
+
+
+
+
+// ETAPE 2
+
+/*let message = "Bonjour !"
+let firstname = "Beyonce"
+let message = "Bonjour !" +" "+ firstname*/
+
+//console.log(message)
+
+
+// ETAPE 3
+
+/*function sayHello(firstname) {
+//let message = "Bonjour !"
+//let firstname = "Beyonce"
+let message = "Bonjour !" +" "+ firstname
+//console.log(message)
+}
+sayHello()
+
+
+
+
+function sayHello(firstname) {
+//let message = "Bonjour !"
+//let firstname = "Beyonce"
+let message = "Bonjour !" +" "+ firstname
+
+console.log(message)
+}
+sayHello("Beyonce")
+
+
+
+// ETAPE 4
+
+function sayHello(firstname,hour)
+
+{
+    if (hour>=18) {
+        console.log( `Bonsoir ${firstname}`)
+    } else {
+        console.log (`Bonjour ${firstname}`)
+    }
+}
+
+sayHello(`Beyonce !`,11)
+sayHello(`Beyonce !`,18)
+sayHello(`Beyonce !`,17)
+
+function sayHello(firstname,hour)
+
+{
+    if (hour>=18) {
+        console.log( `Bonsoir ${firstname}`)
+        
+    } else {
+        console.log (`Bonjour ${firstname}`)
+        
+    }
+}
+
+sayHello(`Beyonce !`,11)
+sayHello(`Beyonce !`,18)
+sayHello(`Beyonce !`,17)
+
+
+// BONUS 6
+
+
+prompt()
+
+/*let firstname = prompt()*/
---
+++ guess_a_number/script.js
@@ -0,0 +1,4 @@
+
+prompt()
+const givenNumber = Number(window.prompt("Veuillez saisir un nombre.", ""));
+function  didIWin (givenNumber)

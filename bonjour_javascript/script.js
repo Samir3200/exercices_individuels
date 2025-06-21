3c225459e16086ee4e87ca2fa430bfa4d483027a
Samir <samirelorf@yahoo.fr>
2025-06-21 11:00:53 +0200

    rectification

--- bonjour_javascript/script.js
+++ bonjour_javascript/script.js
@@ -1,77 +1,33 @@
 
-
-
+// ETAPE 1
+/*let message = "Bonjour !"
+console.log(message)*/
 
 
 // ETAPE 2
-
-/*let message = "Bonjour !"
 let firstname = "Beyonce"
-let message = "Bonjour !" +" "+ firstname*/
+let message = (`Bonjour ! ${firstname}`)
+console.log(message)
 
-//console.log(message)
 
 
 // ETAPE 3
-
-/*function sayHello(firstname) {
-//let message = "Bonjour !"
-//let firstname = "Beyonce"
-let message = "Bonjour !" +" "+ firstname
-//console.log(message)
+function sayHello() {
+let firstname = "Beyonce"
+let message = (`Bonjour ! ${firstname}`)
+console.log(message)
 }
 sayHello()
 
 
-
-
-function sayHello(firstname) {
-//let message = "Bonjour !"
-//let firstname = "Beyonce"
-let message = "Bonjour !" +" "+ firstname
-
-console.log(message)
-}
-sayHello("Beyonce")
-
-
-
 // ETAPE 4
-
-function sayHello(firstname,hour)
-
-{
+function sayHello(firstname,hour) {
     if (hour>=18) {
         console.log( `Bonsoir ${firstname}`)
     } else {
         console.log (`Bonjour ${firstname}`)
     }
 }
-
 sayHello(`Beyonce !`,11)
 sayHello(`Beyonce !`,18)
 sayHello(`Beyonce !`,17)
-
-function sayHello(firstname,hour)
-
-{
-    if (hour>=18) {
-        console.log( `Bonsoir ${firstname}`)
-        
-    } else {
-        console.log (`Bonjour ${firstname}`)
-        
-    }
-}
-
-sayHello(`Beyonce !`,11)
-sayHello(`Beyonce !`,18)
-sayHello(`Beyonce !`,17)
-
-
-// BONUS 6
-
-
-prompt()
-
-/*let firstname = prompt()*/

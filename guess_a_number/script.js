ed0bbce12effe79d9bb01fdf40a54ba50a2d9f3a
Samir <samirelorf@yahoo.fr>
2025-06-22 12:25:03 +0200

    travail

--- guess_a_number/script.js
+++ guess_a_number/script.js
@@ -1,4 +1,37 @@
 
-prompt()
-const givenNumber = Number(window.prompt("Veuillez saisir un nombre.", ""));
-function  didIWin (givenNumber)
+
+
+// Étape 1
+function givenNumber(nombre) {
+  return nombre
+}
+
+
+
+
+let nombreADeviner = 20;
+let essais = 0;
+ //Étape 2
+function didIWin(givenNumber) {
+  if (givenNumber < nombreADeviner) {
+    alert("Plus petit !");
+    return false;
+  } else if (givenNumber > nombreADeviner) {
+    alert("Plus grand !");
+   return false;
+  } else {
+    alert("Bravo ! Vous avez deviné le nombre");
+    return true;
+  }
+}
+console.log(didIWin(20))
+
+
+// Étape 3
+function gamePlay() {
+  let trouve = false;
+  while (!trouve) {
+    const guess = demanderNombre();
+    trouve = verifierNombre(guess);
+  }
+}
